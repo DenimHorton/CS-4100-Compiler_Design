3a9f4a4d5e974c0082ece01c38c4438f62aed187
DenimHorton <denim_horton@hotmail.com>
2022-01-19 19:34:40 -0700

    Brushing up on java, created playground

---
+++ code/playground/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    int x = 5;
+    String name = "Cat";
+
+    public void makeNoise() {
+        if (this.name == "Cat") {
+            System.out.println("MEOW!!!!");
+
+        }
+    }
+}
---
+++ code/playground/palyground.java
@@ -0,0 +1,9 @@
+class palyground {
+    public static void main(String[] args) {
+        Animal myObj = new Animal();
+        System.out.println(myObj.x);
+        myObj.x = 40;
+        System.out.println(myObj.x);
+        myObj.makeNoise();
+    }
+}
