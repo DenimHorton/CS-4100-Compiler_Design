ac0c48e98c8d6abd15842f733f1d46c470a428a3
DenimHorton <denim_horton@hotmail.com>
2022-01-19 20:45:54 -0700

    Testing pipeline

--- code/playground/Animal.java
+++ code/playground/Animal.java
@@ -1,10 +1,12 @@
 public class Animal {
     int x = 5;
     String name = "Cat";
+    String type = "Feline";
 
     public void makeNoise() {
         if (this.name == "Cat") {
             System.out.println("MEOW!!!!");
+            System.out.println(this.type);
 
         }
     }
