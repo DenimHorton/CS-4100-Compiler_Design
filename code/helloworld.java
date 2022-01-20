d3dd44aafcafe5a9419cdfdbfb8a8d869c6cf9d2
DenimHorton <denim_horton@hotmail.com>
2022-01-19 18:41:21 -0700

    Adding in workflow for pipeline and helloworld java program

---
+++ code/helloworld.java
@@ -0,0 +1,6 @@
+public class helloworld {
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+}
