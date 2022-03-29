ff4e98a893ef1d2ceb2cd9b846a6e32e721fac2b
Robert Denim Horton <Denim_horton@hotmail.com>
2022-03-29 07:20:39 -0600

    Added in project 05

---
+++ Projects/Compiler-Part05/SP22HW4/main04.java
@@ -0,0 +1,32 @@
+
+package SP22HW4;
+
+// import ADT.SymbolTable;
+// import ADT.Lexical;
+import ADT.*;
+
+/**
+ *
+ * @author abrouill SPRING 2021
+ */
+public class main04 {
+    public static void main(String[] args) {
+        String fileAndPath = ".\\SP22HW4\\Inputs\\LexicalTestSP22.txt";
+        System.out.println("Lexical for " + fileAndPath);
+        boolean traceOn = true;
+        // Create a symbol table to store appropriate3 symbols found
+        SymbolTable symbolList;
+        symbolList = new SymbolTable(150);
+        Lexical myLexer = new Lexical(fileAndPath, symbolList, traceOn);
+        Lexical.token currToken;
+        currToken = myLexer.GetNextToken();
+        while (currToken != null) {
+            System.out.println("\t" + currToken.mnemonic + " | \t" +
+                    String.format("%04d", currToken.code)
+                    + " | \t" + currToken.lexeme);
+            currToken = myLexer.GetNextToken();
+        }
+        symbolList.PrintSymbolTable(".\\SP22HW4\\Outputs\\symbolTableLex.txt");
+        System.out.println("Done.");
+    }
+}
